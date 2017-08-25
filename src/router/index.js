5585450f1cea67a0dd866c09dc3e2b6c8c10f0b0
voocel <voocel@gmail.com>
2017-08-25 23:56:19 +0800

    resource test

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import goodslist from '@/views/goodslist'
 import Title from '@/views/title'
 import Image from '@/views/image'
 import Cart from '@/views/cart'
+import Resource from '@/views/resource'
 
 Vue.use(Router)
 
@@ -33,6 +34,11 @@ export default new Router({
       path: '/cart',
       name: 'cart',
       component: Cart,
+    },
+    {
+      path: '/resource',
+      name: 'resource',
+      component: Resource,
     }
 
   ]
---
+++ src/views/resource.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+    <link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <div id="app" class="container">
+        <h1>vue-resource</h1>
+        <a class="btn btn-primary" href="javascript:;">GET请求</a>
+        <a class="btn btn-primary" href="javascript:;">post请求</a>
+        <a class="btn btn-primary" href="javascript:;">jsonp请求</a>
+    </div>
+</div>
+</template>
+<script src="node_modules/vue/dist/vue.min.js"></script>
+<script src="node_modules/vue-resource/dist/vue-resource.min.js"></script>
+<script>
+export default {
+  data(){
+      return{
+          msg:"hei!"
+      }
+  }
+}
+</script>
+
